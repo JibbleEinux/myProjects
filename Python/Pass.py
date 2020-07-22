03743ed3417a03b4d11b89ecda369978d93fb758
Jibble Einux <50579963+JibbleEinux@users.noreply.github.com>
2020-07-22 17:02:26 +0600

    Password generator

--- Python/Pass.py
+++ Python/Pass.py
@@ -15,6 +15,10 @@ print('                  This tool create @Jibble Einux                   ')
 print('            follow on github,facebook: @JibbleEinuxCODE            ')
 
 
+# this function make for creating a random authentication Code
+# and also create a random password
+# thats why you can make a easy password , strong password , semi-strong password and extra-strong password
+
 def get_random_string(length):
     letters = string.ascii_lowercase
     result_str = ''.join(random.choice(letters) for i in range(length))
@@ -25,12 +29,20 @@ def get_random_string(length):
     print('Your Password is: ' + passWord)
 
 
+# this function make for finding password if you forgotten
+# if you want to find your password you just enter your random authentication Code / custom authentication code
+# after giving your authentication code this function creating your password and it will be matched your old password
+# Because of when 1st time create your password with this authentication code this time will be same
+
 def find_password_string():
     result_str = input('Enter You Authentication Code: ')
     passWord = (str(len(result_str)) + str(result_str[:2].lower()) + str(result_str[:1:-2].upper()) + "-" + str(result_str.count(result_str)) + str(result_str[:1].upper()) + "X" + "6" + "_" + "D" + str(
         result_str[0::3].lower()) + str(result_str.count(result_str)) + "X" + "f" + str(result_str[:4].lower()) + "+" + str(result_str[4::-1].upper())).strip().replace(" ", "")
     print('Your Password is: ' + passWord)
 
+
+# this section creating for user can make her own authentication code thats why he can remember her authetication code
+# and people can easily find her password thats why make this section
 
 def custom_auth_Code():
     result_str = input('Enter You Custom Authentication Code: ')
